eadb1adfb21ccd8c83504019dcf6c85831f5b244
Jeel Patel <jeel@toddleapp.com>
2022-11-18 00:04:43 +0530

    chore: fix sorting order of user dataloader

--- dataloaders/auth.js
+++ dataloaders/auth.js
@@ -1,7 +1,20 @@
+/**--external-- */
+const _reduce = require('lodash/reduce');
+const _map = require('lodash/map');
+
+/**--relative-- */
 const { findMultipleUsersById } = require('../services/auth/controllers');
 
 const batchLoadUsersByIds = async ({ keys }) => {
-  return await findMultipleUsersById({ ids: keys });
+  const result = await findMultipleUsersById({ ids: keys });
+  const usersById = _reduce(
+    result,
+    (acc, user) => {
+      return { ...acc, [user._id]: user };
+    },
+    {}
+  );
+  return _map(keys, (userId) => usersById[userId]);
 };
 
 module.exports = { batchLoadUsersByIds };
