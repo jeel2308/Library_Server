97f4cccf5f9dd0c9bd19d58f7d9960a75915f991
Jeel Patel <jeel@toddleapp.com>
2022-11-26 18:44:36 +0530

    feat: add util to convert time string in seconds

--- utils/index.js
+++ utils/index.js
@@ -8,6 +8,9 @@ const decodeFromBase64 = ({ text }) => {
   return Buffer.from(text, 'base64').toString('ascii');
 };
 
+/**
+ * @deprecated
+ */
 const generateJwt = ({ user }) => {
   const { JWT_SECRET, ACCESS_TOKEN_EXPIRATION_DURATION } = process.env;
   return jwt.sign({ id: user._id }, JWT_SECRET, {
@@ -21,9 +24,39 @@ const generatePassword = ({ length }) => {
     .substring(2, 2 + length);
 };
 
+/**
+ *
+ * @param {Object} param0
+ * @param {String} param0.time time string like 7d, 1w, 5h
+ * @returns
+ */
+const convertTimeStringInSeconds = ({ time }) => {
+  const numericMultiplier = parseInt(time);
+  const timeSuffix = time[time.length - 1];
+
+  let seconds;
+  switch (timeSuffix) {
+    case 'd': {
+      seconds = 86400;
+      break;
+    }
+    case 'w': {
+      seconds = 7 * 86400;
+      break;
+    }
+    case 'h': {
+      seconds = 3600;
+      break;
+    }
+  }
+
+  return numericMultiplier * seconds;
+};
+
 module.exports = {
   encodeToBase64,
   decodeFromBase64,
   generateJwt,
   generatePassword,
+  convertTimeStringInSeconds,
 };
