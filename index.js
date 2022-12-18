fe6de427bde837cbbd841121b109bd786059e3ff
patel jeel <jeelpatel238@gmail.com>
2022-12-18 18:32:37 +0530

    fix: fix server startup issues

--- index.js
+++ index.js
@@ -16,82 +16,83 @@ const buildDataLoaders = require('./dataloaders');
 const { userRoutes, pingRoutes, cspRoutes } = require('./routes');
 const { verifyToken, globalErrorHandler, setCsp } = require('./middleware');
 
-const app = express();
+const startServer = async (...args) => {
+  console.log('Inside function');
+  const app = express();
 
-dotEnv.config();
+  dotEnv.config();
 
-initMailTransporter();
+  initMailTransporter();
 
-/**
- * This will set Content-Security-Policy for each request
- */
-app.use(setCsp);
+  /**
+   * This will set Content-Security-Policy for each request
+   */
+  app.use(setCsp);
 
-/**
- * This will expose cookies on req.cookies
- */
-app.use(cookieParser());
+  /**
+   * This will expose cookies on req.cookies
+   */
+  app.use(cookieParser());
 
-/**
- * This will allow request from only specific origins
- */
-const whitelist = [
-  'http://localhost:3000',
-  'https://resource-library.netlify.app',
-  'https://studio.apollographql.com',
-];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback({ message: 'Not allowed by CORS', statusCode: 500 });
-    }
-  },
-  credentials: true,
-};
-app.use(cors(corsOptions));
+  /**
+   * This will allow request from only specific origins
+   */
+  const whitelist = [
+    'http://localhost:3000',
+    'https://resource-library.netlify.app',
+    'https://studio.apollographql.com',
+  ];
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback({ message: 'Not allowed by CORS', statusCode: 500 });
+      }
+    },
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
 
-/**
- * This will add JSON payload in as req.body Object
- */
-app.use(
-  express.json({
-    type: [
-      'application/json',
-      'application/csp-report',
-      'application/reports+json',
-    ],
-  })
-);
-/**
- * This will populate URL encoded payload as req.body Object
- */
-app.use(express.urlencoded({ extended: true }));
+  /**
+   * This will add JSON payload in as req.body Object
+   */
+  app.use(
+    express.json({
+      type: [
+        'application/json',
+        'application/csp-report',
+        'application/reports+json',
+      ],
+    })
+  );
+  /**
+   * This will populate URL encoded payload as req.body Object
+   */
+  app.use(express.urlencoded({ extended: true }));
 
-app.use(userRoutes);
+  app.use(userRoutes);
 
-app.use(pingRoutes);
+  app.use(pingRoutes);
 
-app.use(cspRoutes);
+  app.use(cspRoutes);
 
-app.use(verifyToken);
+  app.use(verifyToken);
 
-app.use(globalErrorHandler);
+  app.use(globalErrorHandler);
 
-/**
- * Instead of using app.listen, we are creating httpServer. httpServer requires a function which
- * handles req and res. Here we are using app for that purpose.
- * So, when request arrives control will flow in this way:
- * httpServer -> app -> middleware ->apollo server
- */
-const httpServer = http.createServer(app);
+  /**
+   * Instead of using app.listen, we are creating httpServer. httpServer requires a function which
+   * handles req and res. Here we are using app for that purpose.
+   * So, when request arrives control will flow in this way:
+   * httpServer -> app -> middleware ->apollo server
+   */
+  const httpServer = http.createServer(app);
 
-const { PASSWORD, DB } = process.env;
+  const { PASSWORD, DB } = process.env;
 
-const DB_URL = `mongodb+srv://Jeel2308:${PASSWORD}@cluster0.erkx1.mongodb.net/${DB}?retryWrites=true&w=majority`;
+  const DB_URL = `mongodb+srv://Jeel2308:${PASSWORD}@cluster0.erkx1.mongodb.net/${DB}?retryWrites=true&w=majority`;
 
-const startServer = async () => {
   try {
     await mongoose.connect(DB_URL, {
       useNewUrlParser: true,
@@ -123,9 +124,15 @@ const startServer = async () => {
 
     await httpServer.listen({ port: process.env.PORT });
     console.log('Express server started');
+    // app(...args);
+    console.log(typeof app, 'APP:', app);
+    args.forEach((arg) => {
+      console.log('ARG::', arg);
+    });
   } catch (e) {
     console.log(e);
   }
 };
 
 startServer();
+module.exports = startServer;
